2d30106c4e8562e2f6a74445060ff65450b4fc9f
Maria Redman <mariaredman@Marias-MacBook-Air-2.local>
2022-07-14 11:52:26 -0400

    Learned Strings

---
+++ strings-basic.js
@@ -0,0 +1,18 @@
+const message = 'Hello Javascript Students'
+const msgToLowerCase = message.toLowerCase()
+const msgToUpperCase = message.toUpperCase()
+const messageNoSpace = message.trim()
+const newMessage = message.replace('students', 'Engineers')
+const withSubString = message.substring(9, 14)
+message.startsWith('Hello')
+
+
+
+console.log(message)
+console.log(msgToLowerCase)
+console.log(toUpperCase)
+console.log(messageNoSpace)
+console.log(newMessage)
+console.log(withSubString)
+console.log(messageNoSpace.startsWith('Hello'))
+console.log(messageNoSpace.endsWith('Students'))
